8fb585cd7b997b202130488874aed3da4a104f31
Kamil Powałowski <kamil.powalowski@gmail.com>
2015-08-18 22:22:38 +0200

    Highlighting elements during scroll

--- AKPickerViewSample/AKPickerView/AKPickerView.m
+++ AKPickerViewSample/AKPickerView/AKPickerView.m
@@ -378,6 +378,14 @@
 {
 	if ([self.delegate respondsToSelector:@selector(scrollViewDidScroll:)])
 		[self.delegate scrollViewDidScroll:scrollView];
+    
+    if (self.selectWhileScrolling && self.pickerViewStyle == AKPickerViewStyleFlat) {
+        CGPoint center = [self convertPoint:self.collectionView.center toView:self.collectionView];
+        NSIndexPath *indexPath = [self.collectionView indexPathForItemAtPoint:center];
+        [self.collectionView selectItemAtIndexPath:indexPath animated:!self.highlightAnimationDisabled
+                                    scrollPosition:UICollectionViewScrollPositionNone];
+        self.selectedItem = indexPath.item;
+    }
 
 	[CATransaction begin];
 	[CATransaction setValue:(id)kCFBooleanTrue
